fca7d34a4b0e6d26dfd92603ae8179a79843379a
Bertrand Otoniel <gamerbertrand@gmail.com>
2025-04-07 17:26:35 -0300

    Tratativas de excessões foram adicionadas, garantindo com que erros gerados advindos do input do usuário sejam tratados corretamente, sem afetar a execução do sistema

--- GerenciadorBiblioteca.py
+++ GerenciadorBiblioteca.py
@@ -1,3 +1,4 @@
+import os,time
 from colorama import init, Fore, Style
 init()
 
@@ -17,33 +18,41 @@ class Livros:
         del self.livros[codigo]
 
     def Exibir_todos_os_livros_da_biblioteca (self):
+        os.system('cls')
+        print("Todos os livros da biblioteca:")
         for chave,valor in self.livros.items():
             Emprestado_ou_nao = ""
             if valor["emprestado"] == True:
                 Emprestado_ou_nao = (Fore.RED + "Livro não está disponível para empréstimo"+Style.RESET_ALL)
             else:
                 Emprestado_ou_nao = (Fore.GREEN + "Livro está disponível para empréstimo"+Style.RESET_ALL)
-            print(f"{chave} - {valor["titulo"]} - {valor["autor"]} - {Emprestado_ou_nao}")
+            print(f"\n{chave} - {valor["titulo"]} - {valor["autor"]} - {Emprestado_ou_nao}")
                 
 
     def Pegar_livro_emprestado(self,codigo):
         
         if self.livros[codigo]["emprestado"] == False:
             self.livros[codigo]["emprestado"] = True
-            print (f"Você pegou o livro {self.livros[codigo]["titulo"]} emprestado")
+            
+            print("Lista de livros atualizada:")
+            self.Exibir_todos_os_livros_da_biblioteca()
+            print (f"\nVocê pegou o livro {self.livros[codigo]["titulo"]} emprestado")
+            time.sleep(3)
         else:
             print("Este livro já foi emprestado para outra pessoa")
-        print("Lista de livros atualizada:")
-        self.Exibir_todos_os_livros_da_biblioteca()
-
+            time.sleep(3)
+        
     def Devolver_livro_para_a_biblioteca (self,codigo):
         if self.livros[codigo]["emprestado"] == True:
             self.livros[codigo]["emprestado"] = False
-            print(f"Agracemos a sua devolução. O livro {self.livros[codigo]["titulo"]} foi devolvido")        
+            print("Lista de livros atualizada:")
+            self.Exibir_todos_os_livros_da_biblioteca()
+            print(f"\nAgracemos a sua devolução. O livro {self.livros[codigo]["titulo"]} foi devolvido")
+            time.sleep(3)
         else:        
             print("Acho que houve um engano, este livro ainda está em nossa posse")
-        print("Lista de livros atualizada:")
-        self.Exibir_todos_os_livros_da_biblioteca()
+            time.sleep(3)
+        
     
            
 livro = Livros()
@@ -52,8 +61,16 @@ livro.Adicionar_livros_a_biblioteca(1,"Apologia à Socrátes","Platão",False)
 livro.Adicionar_livros_a_biblioteca(2,"Hábitos atômicos","James Clear",False)
 
 def Perguntar_ao_usuario_o_que_deseja_realizar():
-    Acao_que_usuario_deseja_realizar = int(input(Fore.YELLOW +"Qual ação deseja realizar? Digite apenas o número da ação desejada\n"+Style.RESET_ALL+" 1 - Ver livros disponíveis na biblioteca\n 2 - Pegar um livro emprestado\n 3 - Devolver um Livro\n 4 - Finalizar \n"))
-    return Acao_que_usuario_deseja_realizar
+    
+    try:
+        Acao_que_usuario_deseja_realizar = int(input(Fore.YELLOW +"\nQual ação deseja realizar? Digite apenas o número da ação desejada\n"+Style.RESET_ALL+" 1 - Ver livros disponíveis na biblioteca\n 2 - Pegar um livro emprestado\n 3 - Devolver um Livro\n 4 - Finalizar \n"))
+        return Acao_que_usuario_deseja_realizar
+    except:
+        os.system('cls')
+    print(Fore.RED+"ERRO!!!"+Style.RESET_ALL+ " Digite apenas o número inteiro correspondente a função que deseja realizar")
+    time.sleep(3)
+
+os.system('cls')    
 Acao_que_usuario_deseja_realizar = Perguntar_ao_usuario_o_que_deseja_realizar()
 
 while Acao_que_usuario_deseja_realizar != 4:
@@ -62,16 +79,18 @@ while Acao_que_usuario_deseja_realizar != 4:
         livro.Exibir_todos_os_livros_da_biblioteca()
     elif Acao_que_usuario_deseja_realizar == 2: #Pegar um livro emprestado
         livro.Exibir_todos_os_livros_da_biblioteca()
-        livro.Pegar_livro_emprestado(int(input("Digite o código do livro que deseja pegar emprestado:\n")))
+        try:
+            livro.Pegar_livro_emprestado(int(input("\nDigite o código do livro que deseja pegar emprestado:\n")))
+        except:
+            print(Fore.RED+"ERRO!!!"+Style.RESET_ALL+ " Digite apenas o número inteiro relativo ao livro que deseja pegar emprestado")
     elif Acao_que_usuario_deseja_realizar == 3: #Devolver um livro
         livro.Exibir_todos_os_livros_da_biblioteca()
-        livro.Devolver_livro_para_a_biblioteca(int(input("Digite o código do livro que deseja devolver:\n")))
+        try:
+            livro.Devolver_livro_para_a_biblioteca(int(input("\nDigite o código do livro que deseja devolver:\n")))
+        except:
+            print(Fore.RED+"ERRO!!!"+Style.RESET_ALL+ "Digite apenas o número inteiro relativo ao livro que deseja devolver")
     Acao_que_usuario_deseja_realizar = Perguntar_ao_usuario_o_que_deseja_realizar()
 
 
 
-
-
-
-# Aprender a usar excessão
 # Perguntar se você é usuário ou "dono"
